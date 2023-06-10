0336263a49c79b9bfae66266635fd99827a01df3
Farouk Ajayi <79684324+FaroukAj@users.noreply.github.com>
2023-06-10 01:01:01 -0500

    Add files via upload
    Project Description:
    
    "📊 Employee Dataset Analysis Project: Unlocking Insights with MySQL! 🚀
    
    In this data analysis project, I tackled an employee dataset using advanced MySQL techniques to extract valuable insights and drive informed decision-making.
    
    🔍 Task 1: Department Age Analysis
    Found the average age of employees in each department, providing valuable demographic insights for effective workforce planning.
    
    📈 Task 2: Cumulative Length of Service
    Calculated the cumulative length of service for each employee, ordered by their hire date. This analysis revealed valuable insights into employee tenure and loyalty.
    
    🌆 Task 3: Top Employee Cities
    Identified the top 5 cities with the highest number of employees, enabling strategic workforce allocation and resource optimization.
    
    🔢 Task 4: Gender Diversity Analysis
    Calculated the percentage of male and female employees in each department, providing insights into gender representation and fostering an inclusive workplace.
    
    🔍 Task 5: Common Job Titles for Terminated Employees
    Identified the most common job title among terminated employees, shedding light on potential patterns or issues related to job positions.
    
    📈 Task 6: Average Age Analysis by Store
    Calculated the average age of employees at each store, considering only active employees. This analysis provided insights into workforce demographics at different store locations.
    
    ⏳ Task 7: Employee Tenure Analysis
    Determined the tenure of employees by calculating the difference between their termination date and hire date. This analysis shed light on the length of employment for each individual.
    
    📊 Task 8: Hiring Trends Analysis
    Calculated the number of employees hired in each year, enabling a comprehensive understanding of hiring trends and patterns over time.
    
    📆 Task 9: Record Update Analysis
    Found the latest record date for each employee and calculated the number of days since their last record update. This analysis ensured data accuracy and identified outdated employee information.
    
    🔢 Task 10: Average Length of Service Analysis by Business Unit
    Determined the average length of service for terminated employees in each business unit, providing valuable insights into retention patterns within different units.
    
    Stay tuned as I continue to refine and enhance this data analysis project. Together, let's leverage the power of data to drive organizational success! 💡"
    
    This professional project description highlights the tasks performed and the insights gained from analyzing the employee dataset using advanced MySQL techniques. It emphasizes the strategic value of the analysis and the potential impact on decision-making and organizational success.

---
+++ employeeproject.sql
@@ -0,0 +1,85 @@
+USE wfsdata;
+
+/* ALTER TABLE emloyeedata RENAME employeedata; */
+
+-- Find the latest record date for each employee and calculate the number of days since their last record update.
+WITH latest_record AS (
+SELECT EmployeeID, recorddate_key, ROW_NUMBER() OVER (PARTITION BY EmployeeID ORDER BY recorddate_key DESC) AS record_date_count
+FROM Employeedata
+)
+SELECT EmployeeID, str_to_date(recorddate_key, '%m/%d/%y' )
+FROM latest_record
+WHERE record_date_count =1;
+
+-- Find the average age of employees in each department.
+SELECT department_name, ROUND(AVG(age)) AS AVG_Age
+FROM employeedata
+GROUP BY department_name;
+
+-- Calculate the cumulative length of service for each employee, ordered by their hire date.
+SELECT employeeID, SUM(length_of_service) AS cumulative_length_of_service
+FROM employeedata
+GROUP BY employeeID;
+
+-- Determine the top 5 cities with the highest number of employees.
+SELECT city_name, COUNT(EmployeeID) AS Employee_count
+FROM employeedata
+GROUP BY city_name
+ORDER BY Employee_counT DESC
+LIMIT 5;
+-- Calculate the percentage of male and female employees in each department.
+WITH employee_percentage AS (
+
+SELECT department_name,
+COUNT(gender_full) AS total_count,
+COUNT(CASE WHEN gender_full ="Male" THEN gender_full END) AS male_count,
+COUNT(CASE WHEN gender_full ="Female" THEN gender_full END) AS female_count
+FROM employeedata
+GROUP BY department_name 
+)
+SELECT department_name, total_count, ROUND((male_count/total_count) * 100.00, 2) AS male_percentage, 
+ROUND((female_count/total_count) * 100.00, 2) AS female_percentage
+FROM employee_percentage;
+								/* OR USING PARTITION BY */ 
+SELECT department_name, gender_full,
+       ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (PARTITION BY department_name),2) AS gender_percentage
+FROM employeedata
+GROUP BY department_name, gender_full;
+ 
+-- Find the most common job title among laid-off employees.
+SELECT job_title, COUNT(EmployeeID) AS'employees_laid-off'
+FROM employeedata
+WHERE termreason_desc = "layoff"
+GROUP BY job_title
+ORDER BY COUNT(EmployeeID);
+
+-- Calculate the average age of employees at each store, considering only active employees.
+SELECT store_name, ROUND(AVG(age))
+FROM employeedata
+WHERE status = "ACTIVE"
+GROUP BY store_name;
+
+-- Determine the tenure of employees by calculating the difference between their termination date and hire date.
+SELECT EmployeeID, orighiredate_key, terminationdate_key,
+DATEDIFF(terminationdate_key, orighiredate_key) AS tenure
+FROM employeedata;
+
+
+
+-- Calculate the number of employees hired in each year.
+/*UPDATE employeedata
+SET orighiredate_key = (select str_to_date(orighiredate_key, '%m/%d/%Y')); */
+
+SELECT   YEAR(orighiredate_key) AS Year_of_hire, COUNT(employeeID) AS Employee_count
+FROM Employeedata
+GROUP BY  YEAR(orighiredate_key)
+ORDER BY Employee_count DESC;
+
+
+-- Determine the average length of service for terminated employees in each business unit
+SELECT Business_unit, AVG(length_of_service) AS avg_service_length
+FROM employeedata
+WHERE STATUS = 'TERMINATED'
+GROUP BY Business_unit;
+
+SELECT * FROM employeedata;
