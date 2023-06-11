1af8bf9b0d8e0030bfd757eb0268bc1b7a994a9e
Farouk Ajayi <79684324+FaroukAj@users.noreply.github.com>
2023-06-11 03:58:56 -0500

    Add files via upload
    Healthcare Claims Data Analysis
    
    This project focuses on performing comprehensive data analysis on a healthcare claims dataset using advanced SQL techniques. The goal is to extract valuable insights and inform decision-making in the healthcare industry. The project involves a series of tasks including analyzing patient demographics, identifying common medical diagnoses, assessing financial metrics, and evaluating service utilization.
    
    Key Tasks:
    - Analyzing patient demographics, such as gender distribution and age groups, to gain insights for targeted healthcare strategies.
    - Identifying the most common medical diagnoses to enable proactive measures for addressing prevalent health issues.
    - Calculating financial metrics including total claimed and paid amounts by insurance companies to support cost management and revenue optimization.
    - Evaluating the average time taken for claims processing to identify opportunities for streamlining operations and improving efficiency.
    - Analyzing service utilization patterns by examining total claimed amounts for different service types.
    - Identifying providers with the highest claimed amounts to facilitate negotiation and contracting strategies.
    - Developing actionable insights and data-driven recommendations to improve decision-making in healthcare claims management.
    
    This project showcases the application of SQL queries and techniques for data analysis in the healthcare domain. By leveraging the power of SQL, it provides valuable insights and aids in improving operational efficiency and decision-making in healthcare claims management.

---
+++ healthinsuranceclaims.sql
@@ -0,0 +1,66 @@
+USE wfsdata;
+SHOW COLUMNS FROM claims_data;
+
+
+-- Calculate the average age of patients in the dataset.
+SELECT ROUND(AVG(AGE))
+FROM claims_data;
+-- Determine the gender distribution of patients in the dataset.
+SELECT SEX, COUNT(SEX) AS gender_distribution
+FROM claims_data
+GROUP BY SEX;
+
+/* OR USING PARTITION BY */
+
+SELECT DISTINCT SEX, COUNT(SEX) OVER (PARTITION BY SEX) AS gender_distribution
+FROM claims_data;
+
+
+-- Identify the most common medical diagnosis in the dataset.
+WITH diagnosis_countt AS (
+SELECT DISTINCT DIAGNOSIS, COUNT(DIAGNOSIS) OVER (PARTITION BY DIAGNOSIS) AS Diagnosis_count
+FROM claims_data
+ORDER BY Diagnosis_count DESC)
+SELECT * 
+FROM diagnosis_countt
+LIMIT 1;
+
+-- Find the total amount paid by each company.
+UPDATE claims_data
+SET TOTAL_PRICE  = (Select CAST(TOTAL_PRICE AS DECIMAL(10,2)));
+
+SELECT company, SUM(TOTAL_PRICE) AS Total_amount
+FROM claims_data
+GROUP BY company;
+
+
+-- Calculate the total claimed amount for each service type.
+SELECT service_type, SUM(TOTAL_PRICE) AS Total_amount
+FROM claims_data
+GROUP BY service_type;
+
+-- Identify the top 5 providers with the highest total claimed amount.
+SELECT Provider_name, SUM(TOTAL_PRICE) AS Total_amount
+FROM claims_data
+GROUP BY Provider_name
+ORDER BY Total_amounT DESC
+LIMIT 5;
+
+-- Find the total number of claims per patient age group (e.g., 0-18, 19-30, 31-45, etc.).
+SELECT CASE
+         WHEN AGE BETWEEN 0 AND 18 THEN '0-18'
+         WHEN AGE BETWEEN 19 AND 30 THEN '19-30'
+         WHEN AGE BETWEEN 31 AND 45 THEN '31-45'
+         ELSE 'Over 45'
+       END AS AgeGroup,
+       COUNT(*) AS Claim_count
+FROM claims_data
+GROUP BY AgeGroup
+ORDER BY Claim_count DESC;
+-- Determine the average total price of medical claims by plan.
+SELECT PLAN, AVG(TOTAL_PRICE) AS Average_Total_Price
+FROM claims_data
+GROUP BY PLAN;
+
+
+SELECT * FROM claims_data;
