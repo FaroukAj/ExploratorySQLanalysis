568bd7b4945f89fe6dfa7ff36012650e72c98b45
Farouk Ajayi <79684324+FaroukAj@users.noreply.github.com>
2023-06-15 02:16:06 -0500

    healthinsuranceclaims.sql

--- healthinsuranceclaims.sql
+++ healthinsuranceclaims.sql
@@ -5,14 +5,18 @@ SHOW COLUMNS FROM claims_data;
 -- Calculate the average age of patients in the dataset.
 SELECT ROUND(AVG(AGE))
 FROM claims_data;
+
+
 -- Determine the gender distribution of patients in the dataset.
-SELECT SEX, COUNT(SEX) AS gender_distribution
+SELECT SEX, 
+       COUNT(SEX) AS gender_distribution
 FROM claims_data
 GROUP BY SEX;
 
 /* OR USING PARTITION BY */
 
-SELECT DISTINCT SEX, COUNT(SEX) OVER (PARTITION BY SEX) AS gender_distribution
+SELECT DISTINCT SEX, 
+       COUNT(SEX) OVER (PARTITION BY SEX) AS gender_distribution
 FROM claims_data;
 
 
@@ -25,22 +29,29 @@ SELECT *
 FROM diagnosis_countt
 LIMIT 1;
 
+
+
 -- Find the total amount paid by each company.
 UPDATE claims_data
 SET TOTAL_PRICE  = (Select CAST(TOTAL_PRICE AS DECIMAL(10,2)));
 
-SELECT company, SUM(TOTAL_PRICE) AS Total_amount
+SELECT company, 
+       SUM(TOTAL_PRICE) AS Total_amount
 FROM claims_data
 GROUP BY company;
 
 
 -- Calculate the total claimed amount for each service type.
-SELECT service_type, SUM(TOTAL_PRICE) AS Total_amount
+SELECT service_type, 
+       SUM(TOTAL_PRICE) AS Total_amount
 FROM claims_data
 GROUP BY service_type;
 
+
+
 -- Identify the top 5 providers with the highest total claimed amount.
-SELECT Provider_name, SUM(TOTAL_PRICE) AS Total_amount
+SELECT Provider_name, 
+       SUM(TOTAL_PRICE) AS Total_amount
 FROM claims_data
 GROUP BY Provider_name
 ORDER BY Total_amounT DESC
@@ -57,6 +68,8 @@ SELECT CASE
 FROM claims_data
 GROUP BY AgeGroup
 ORDER BY Claim_count DESC;
+
+
 -- Determine the average total price of medical claims by plan.
 SELECT PLAN, AVG(TOTAL_PRICE) AS Average_Total_Price
 FROM claims_data
